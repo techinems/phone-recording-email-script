0eb94954ef5a38fdfc4338a401d0d41cb4091d23
Dan Bruce <ddbruce@users.noreply.github.com>
2021-10-02 17:16:12 -0400

    Remove from address

--- script.sh
+++ script.sh
@@ -10,8 +10,10 @@
 # $7 - call destination
 # $dt - datetime string
 # $email - email address to send text and file
+# $from - the "from" email address
 
 email="email@example.com" #change this line
+from="noreply@example.com" #change this one too
 
 #fill in the extension you wish to send emails on in the below quotes
 if [ $7 != "" ]; then
@@ -24,4 +26,4 @@ dt=$(date -d '+3 hours' '+%d %b %y %H:%M:%S'); #+3 hours because our server sits
 
 echo -e "New recording from the duty sup line:\n
 Date/time: $dt
-From: $5" | mail -A /var/spool/asterisk/monitor/$1/$2/$3/$6 -s "Call on the duty sup line from $5" -a "From:noreply@dsparkman.com" $email
+From: $5" | mail -A /var/spool/asterisk/monitor/$1/$2/$3/$6 -s "Call on the duty sup line from $5" -a "From:$from" $email
